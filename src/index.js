754891b47c1f03564637d9bc3ce0e056f0d65a90
Ibrahim <abram.pomposelli1@gmail.com>
2022-05-21 15:29:53 +0200

    Update tailwind config

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 // javascript
-import React from "react"
-import ReactDOM from "react-dom"
-import Header from "./header.js"
-import Footer from "./footer.js"
-import MainContent from "./main-content.js"
-
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./header.js";
+import Footer from "./footer.js";
+import MainContent from "./main-content.js";
+import "./index.css";
 
 function Page() {
-    return (
-        <div className="main-div">
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    )
+  return (
+    <div className="main-div">
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
 }
 
-ReactDOM.render(<Page />, document.getElementById("message"))
+ReactDOM.render(<Page />, document.getElementById("message"));
